eae7985f83ac3814bde811da63ba1ec188952fcb
unknown <caliskancaglayan@gmail.com>
2022-01-13 20:25:21 +0300

    init router and icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Card from './components/shared/Card';
 import { v4 as uuid } from 'uuid';
 import React, { useState } from 'react';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
+import AboutIconLink from './components/AboutIconLink';
+
+////////
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -24,14 +30,30 @@ function App() {
     setFeedback([newFeedback, ...feedback]);
   };
   return (
-    <>
+    <Router>
       <Header />
       <div className='container'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+
+        <AboutIconLink />
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -9,6 +9,7 @@ const FeedbackData = [
     rating: 9,
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
+
   {
     id: 3,
     rating: 8,
